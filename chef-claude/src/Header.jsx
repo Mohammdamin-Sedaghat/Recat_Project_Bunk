6e97a0fdd24c506e2e2dddf802acaec9402c5c11
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-01-08 22:34:51 -0500

    implemented state

---
+++ chef-claude/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+  return (
+    <header>
+      <img className="logo" src="../images/logo.png" />
+      <span>Chef Claude</span>
+    </header>
+  )
+}
--- chef-claude/src/main.jsx
+++ chef-claude/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { useState } from "react"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function Main() {
+    const [ingredients, setIngredients] = useState(["oregano", "tomato", "black"])
+
+    const ingredientListItem = ingredients.map(ingredient => {
+        return <li key={ingredient}>{ingredient}</li>
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const ingredientName = formData.get("ingredient")
+        
+        setIngredients(prevIng => [...prevIng, ingredientName])
+    }
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    aria-label="Add ingredient" 
+                    placeholder="e.g. oregano"
+                    name="ingredient" 
+                />
+                <button>+ Add Ingrediant</button>
+            </form>
+
+            <ul>
+                { ingredientListItem }
+            </ul>
+        </main>
+    )
+}
