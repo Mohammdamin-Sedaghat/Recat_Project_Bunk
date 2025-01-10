bc5a3f51f3777f0b2b18c77a370028e1a235c245
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-01-09 23:14:19 -0500

    implemented conditional rendering

--- chef-claude/src/main.jsx
+++ chef-claude/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Main() {
 
-    const [ingredients, setIngredients] = React.useState(["chicken", "cum"])
+    const [ingredients, setIngredients] = React.useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient} className="ingredient_list">{ingredient}</li>
@@ -21,22 +21,27 @@ export default function Main() {
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                     name="ingredient"
+                    required
                 />
                 <button>Add ingredient</button>
             </form>
-            <section>
-                <h2>Ingredients on hand:</h2>
-                <ul>
-                    {ingredientsListItems}
-                </ul>
-                <div className="recepie_box">
-                    <div>
-                        <p className="ready_for_recepie">Ready for a recepie?</p>
-                        <p className="generate_text">Generate a recepie from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recepie</button>
-                </div>
-            </section>
+            {ingredients.length > 0  && 
+                <section>
+                    <h2>Ingredients on hand:</h2>
+                    <ul>
+                        {ingredientsListItems}
+                    </ul>
+                    {ingredients.length > 4 &&
+                        <div className="recepie_box">
+                            <div>
+                                <p className="ready_for_recepie">Ready for a recepie?</p>
+                                <p className="generate_text">Generate a recepie from your list of ingredients.</p>
+                            </div>
+                            <button>Get a recepie</button>
+                        </div>
+                    }
+                </section>
+            }
             
         </main>
     )
