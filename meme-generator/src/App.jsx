e3cb1af84e991136403fd5bd9cec0a734968904a
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-01-13 10:50:02 -0500

    started

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import Main from "./Main"
+import Header from "./Header"
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <Main />
+    <Header />
+  </>
+)
--- meme-generator/src/main.jsx
+++ meme-generator/src/main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
-
-createRoot(document.getElementById('root')).render(
-  <h2>Hi</h2>
-)
+export default function App() {
+    return (
+        <h2>This is Main</h2>
+    )
+}
