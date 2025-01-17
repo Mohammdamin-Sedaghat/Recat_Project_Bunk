1c79952d0b0aa436e069068c1016c11f945c3998
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-01-17 18:24:12 -0500

    added dice and rolling

--- Tenziz/src/App.jsx
+++ Tenziz/src/App.jsx
@@ -1,7 +1,27 @@
+import Die from "./Die"
+import { useState } from "react"
+
 export default function App() {
+    const [dieValues, setDieValues] = useState(generateAllNewDie())
+
+    function generateAllNewDie() {
+        return new Array(10)
+            .fill(0)
+            .map(()=> Math.ceil(Math.random() * 6))
+    }
+
+    function rollNewDice() {
+        setDieValues(generateAllNewDie())
+    }
+    
+    const dieElements = dieValues.map((dieVal, i) => <Die key={i} value={dieVal} />)
+
     return (
         <main>
-        
+            <div className="die-container">
+                {dieElements}
+            </div>
+            <button className="roll-butt" onClick={rollNewDice}>Roll</button>
         </main>
     )
 }
