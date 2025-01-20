059c573d1e0620c52eb2de5363b0198d267115bf
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-01-19 22:30:07 -0500

    made the header

--- Assembly-Endgame/src/App.jsx
+++ Assembly-Endgame/src/App.jsx
@@ -1,10 +1,29 @@
-import { createRoot } from "react-dom/client"
-import Main from "./Main"
-import Header from "./Header"
+import languages from "../languages.json"
 
-createRoot(document.querySelector("#root")).render(
-  <>
-    <Header />
-    <Main />
-  </>
-)
+export default function App() {
+  const languageElments = languages.map((language, i) => {
+    return (
+      <span
+        key={i}
+        style={{backgroundColor: language.backgroundColor, color:language.color}}
+      >
+        {language.name}
+      </span>)
+  })
+  
+  return (
+    <main>
+      <header>
+          <h1>Assembly Endgame</h1>
+          <p className="header-text">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className="state">
+        <span>You win!</span>
+        <span style={{fontSize:"1rem"}}>Well done! 🎉</span>
+      </section>
+      <section className="languages">
+        {languageElments}
+      </section>
+    </main>
+  )
+}
--- Assembly-Endgame/src/Main.jsx
+++
@@ -1,5 +0,0 @@
-export default function Main() {
-    return (
-        <h1>Pus</h1>
-    )
-}
