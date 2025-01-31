cece6fa868401a22713dfebe3155f0a927e4abfc
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-01-30 20:57:49 -0500

    feat: Add fallback metadata to layout

--- flowrise-prismic/src/app/page.tsx
+++ flowrise-prismic/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="font-display text-5xl font-bold">
-      <h1>Hello World!</h1>
+      <h1>Hello World!!</h1>
     </main>
   );
 }
